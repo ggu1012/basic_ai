24f264d59909e5f9456899eb6ae4a8dade642521
ggu1012 <ggu1012@naver.com>
2021-12-16 23:17:21 +0900

    Project 9 finished - ALL FINISHED!!

--- project9/project9.py
+++ project9/project9.py
@@ -187,16 +187,18 @@ class MLP(nn.Module):
     def __init__(self):
         super().__init__()
         ### CODE HERE ###
-        self.fc1 = nn.Linear(in_features=64, out_features=8)
-        self.fc2 = nn.Linear(in_features=8, out_features=7)
+        self.fc1 = nn.Linear(in_features=1024, out_features=64)
+        self.fc2 = nn.Linear(in_features=64, out_features=8)
+        self.fc3 = nn.Linear(in_features=8, out_features=7)
         #################
 
 
     def forward(self, x):
         x = x.view(-1, img_size * img_size)
         ### CODE HERE ###
-        x = torch.relu(self.fc1)
-        x = torch.relu(self.fc2)
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = torch.relu(self.fc3(x))
         #################
         return x
     
@@ -225,10 +227,10 @@ def train(model, optimizer, criterion, train_dataset, total_epoch):
             optimizer.zero_grad()
     
             # forward propagation
-            output = MLP(X_train)
+            output = model.forward(image_batch)
 
             # calculate loss
-            loss = criterion(output, y_train)
+            loss = criterion(output, label_batch)
             
             # backward propagation
             loss.backward()
@@ -284,14 +286,29 @@ class DeeperCNN(nn.Module):
         super().__init__()
         
         ### CODE HERE ###
-        raise NotImplementedError("Erase this line and write down your code.")
+        self.conv1 = nn.Conv2d(1, 4, kernel_size=3, padding=1, stride=1)
+        self.conv2 = nn.Conv2d(4, 4, kernel_size=3, padding=1, stride=1)
+        self.conv3 = nn.Conv2d(4, 8, kernel_size=3, padding=1, stride=1)
+        self.conv4 = nn.Conv2d(8, 8, kernel_size=3, padding=1, stride=1)
+        self.conv5 = nn.Conv2d(8, 16, kernel_size=3, padding=1, stride=1)
+        self.conv6 = nn.Conv2d(16, 16, kernel_size=3, padding=1, stride=1)
+        self.fc1 = nn.Linear(16, 8)
+        self.fc2 = nn.Linear(8, 7)
         #################
 
     def forward(self, x):
-        x = x.view(-1, 1, img_size, img_size)
+        x = x.view(-1, 1, img_size, img_size)        
         
         ### CODE HERE ###
-        raise NotImplementedError("Erase this line and write down your code.") 
+        x = F.relu(self.conv1(x))
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = F.relu(F.max_pool2d(self.conv3(x), 2))
+        x = F.relu(F.max_pool2d(self.conv4(x), 2))
+        x = F.relu(F.max_pool2d(self.conv5(x), 2))
+        x = F.relu(F.max_pool2d(self.conv6(x), 2))
+        x = x.view(-1, 16)
+        x = F.relu(self.fc1(x))
+        logits = self.fc2(x)
         #################
         
         return logits
